14bb44f9e8486da1421237e8c07d0f06970a747f
rooomaisa <romyvandam334@gmail.com>
2024-12-22 12:29:20 +0100

    creating pokemon objects

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        ElectricPokemon pikachu = new ElectricPokemon("Pikachu", 5, 25, 25, "Electric", 15, 25);
+        FirePokemon charmander = new FirePokemon("Charmander", 5, 25, 25, "Fire", 10, 15);
+        WaterPokemon squirtle = new WaterPokemon("Squirtle", 5, 30, 20, "Water", 8, 18);
+        GrassPokemon bulbasaur = new GrassPokemon("Bulbasaur", 5, 35, 15, "Grass", 12, 10);
+
+
+
+
+
     }
 }
--- src/WaterPokemon.java
+++ src/WaterPokemon.java
@@ -1,4 +1,43 @@
-public class WaterPokemon {
-    private String waterShield;
-    private String hydrationLevel;
+public class WaterPokemon extends Pokemon {
+    private int waterShield;
+    private int hydrationLevel;
+
+
+    public WaterPokemon(String name, int level, int hp, int xp, String type, int waterShield, int hydrationLevel) {
+        super(name, level, hp, xp, type);
+        this.waterShield = waterShield;
+        this.hydrationLevel = hydrationLevel;
+    }
+
+
+    public int getWaterShield() {
+        return waterShield;
+    }
+
+    public void setWaterShield(int waterShield) {
+        this.waterShield = waterShield;
+    }
+
+
+    public int getHydrationLevel() {
+        return hydrationLevel;
+    }
+
+    public void setHydrationLevel(int hydrationLevel) {
+        this.hydrationLevel = hydrationLevel;
+    }
+
+
+    @Override
+    public void attack() {
+        super.attack();
+        System.out.println(getName() + " uses Hydro Pump! It’s super effective!");
+        setXp(getXp() + 20);
+        System.out.println(getName() + " gained an additional 20 XP! Total XP: " + getXp());
+    }
+
+
+    public void splash() {
+        System.out.println(getName() + " splashes water everywhere!");
+    }
 }
